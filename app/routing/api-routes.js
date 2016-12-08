c1b21d1ea5d75306ec787f5af7cc6202edb32117
Ryan Ngoh <rxn120230@utdallas.edu>
2016-12-08 09:02:19 -0600

    fixed ajax post and api post route

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -8,7 +8,17 @@ module.exports = function(app){
 	});
 	//API post request to submit data into the friends.js 
 	app.post("/api/friends", function(req, res){
+		var matchesArray = [];
+		for(var j = 0; j < friendData.length; j++){
+			var score = 0;
+	    	for(var i = 0; i < 10; i++){
+	    		score += Math.abs(parseInt(req.body.score[i]) - friendData[j].score[i]);
+	    	}
+	    	matchesArray.push(parseInt(score));
+		}
+		var minMatch = Math.min.apply(null, matchesArray);
+		var theMatch = matchesArray.indexOf(minMatch);
+		res.json(friendData[theMatch]);
 		friendData.push(req.body);
-		res.json(true);
 	});
 };
